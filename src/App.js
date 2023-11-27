71f77453731a0aa70b18ee397cebb5a9dd30569a
roquebernedo <roquebernedo@gmail.com>
2023-11-27 00:15:17 -0500

    added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router } from "react-router-dom";
 import Presentacion from './Presentacion';
 import Contact from './Contact';
--- src/Skills.jsx
+++ src/Skills.jsx
@@ -61,7 +61,6 @@ const Skills = () => {
              
             {!showElement1 && !showElement2 && !showElement3 && (
             <div className="resume-burger">
-                
                     <div className='text'>
                         "Programming is like a digital burger, full of layers of code that combine to create a delicious 
                         experience for the user." 
@@ -70,7 +69,6 @@ const Skills = () => {
                     <div className="text-select">
                         Select a layer on the burger to show stack skills.
                     </div>
-                
             </div>
             )}
             {showElement1 && (<div className="tools-burger">
@@ -80,28 +78,28 @@ const Skills = () => {
                     </div>
                 </div>
                 <div className='top-fav'>
-                    <div className="text-select">
-                        <div className='box-java'>
-                            <img className='jav' src={(require('./images/javascript.png'))} alt=''/>
-                            <div className='name-java'>JavaScript</div>
+                    <div className="text-select-all">
+                        <div className='box-java common-box'>
+                            <div className='div-icon'><img className='jav' src={(require('./images/javascript.png'))} alt=''/></div>
+                            <div className='name-java common-name'>JavaScript</div>
                         </div>
-                        <div className='box-react'>
-                            <img className='react' src={(require('./images/react.png'))} alt=''/>
-                            <div className='name-react'>React</div>
-                        </div>    
-                        <div className='box-redux'>
-                            <img className='redux' src={(require('./images/redux.png'))} alt=''/>
-                            <div className='name-redux'>Redux</div>
+                        <div className='box-react common-box'>
+                            <div className='div-icon'><img className='react' src={(require('./images/react.png'))} alt=''/></div>
+                                <div className='name-react common-name'>React</div>
+                            </div>    
+                        <div className='box-redux common-box'>
+                            <div className='div-icon'><img className='redux' src={(require('./images/redux.png'))} alt=''/></div>
+                            <div className='name-redux common-name'>Redux</div>
                         </div>     
                     </div>
-                    <div className="text-select2">
-                        <div className='box-html'>
-                            <img className='html' src={(require('./images/html5.png'))} alt=''/>
-                            <div className='name-html'>HTML5</div>
+                    <div className="text-select-all">
+                        <div className='box-html common-box'>
+                            <div className='div-icon'><img className='html' src={(require('./images/html5.png'))} alt=''/></div>
+                            <div className='name-html common-name'>HTML5</div>
                         </div>
-                        <div className='box-css'>
-                            <img className='css' src={(require('./images/css.png'))} alt=''/>
-                            <div className='name-css'>CSS3</div>
+                        <div className='box-css common-box'>
+                            <div className='div-icon'><img className='css' src={(require('./images/css.png'))} alt=''/></div>
+                            <div className='name-css common-name'>CSS3</div>
                         </div>
                     </div>
                 </div>
@@ -118,29 +116,29 @@ const Skills = () => {
                     </div>
                 </div>
                 <div className='top-fav'>
-                    <div className="text-select">
-                        <div className='box-git'>
-                            <img className='git' src={(require('./images/git.png'))} alt=''/>
-                            <div className='name-git'>Git</div>
+                    <div className="text-select-all">
+                        <div className='box-git common-box'>
+                            <div className='div-icon'><img className='git' src={(require('./images/git.png'))} alt=''/></div>
+                            <div className='name-git common-name'>Git</div>
                         </div>
-                        <div className='box-heroku'>
-                            <img className='heroku' src={(require('./images/heroku.png'))} alt=''/>
-                            <div className='name-heroku'>Heroku</div>
+                        <div className='box-heroku common-box'>
+                            <div className='div-icon'><img className='heroku' src={(require('./images/heroku.png'))} alt=''/></div>
+                            <div className='name-heroku common-name'>Heroku</div>
                         </div>    
                           
                     </div>
-                    <div className="text-select2">
-                        <div className='box-html'>
-                            <img className='html' src={(require('./images/id.png'))} alt=''/>
-                            <div className='name-html'>OAuth</div>
+                    <div className="text-select-all">
+                        <div className='box-oauth common-box'>
+                            <div className='div-icon'><img className='oauth' src={(require('./images/id.png'))} alt=''/></div>
+                            <div className='name-oauth common-name'>OAuth</div>
                         </div>
-                        <div className='box-css'>
-                            <img className='css' src={(require('./images/vercel.png'))} alt=''/>
-                            <div className='name-css'>Vercel</div>
+                        <div className='box-vercel common-box'>
+                            <div className='div-icon'><img className='vercel' src={(require('./images/vercel.png'))} alt=''/></div>
+                            <div className='name-vercel common-name'>Vercel</div>
                         </div>
-                        <div className='box-css'>
-                            <img className='css' src={(require('./images/netlify.jpg'))} alt=''/>
-                            <div className='name-css'>Netlify</div>
+                        <div className='box-netlify common-box'>
+                            <div className='div-icon'><img className='netlify' src={(require('./images/netlify.jpg'))} alt=''/></div>
+                            <div className='name-netlify common-name'>Netlify</div>
                         </div>
                     </div>
                 </div>
@@ -157,33 +155,33 @@ const Skills = () => {
                     </div>
                 </div>
                 <div className='top-fav'>
-                    <div className="text-select-back">
-                        <div className='box-express'>
-                            <img className='express back' src={(require('./images/express.png'))} alt=''/>
-                            <div className='name-express'>Express</div>
+                    <div className="text-select-all">
+                        <div className='box-express common-box'>
+                            <div className='div-icon'><img className='express back' src={(require('./images/express.png'))} alt=''/></div>
+                            <div className='name-express common-name'>Express</div>
                         </div>
-                        <div className='box-node'>
-                            <img className='node back' src={(require('./images/node.png'))} alt=''/>
-                            <div className='name-node'>Node</div>
+                        <div className='box-node common-box'>
+                            <div className='div-icon'><img className='node back' src={(require('./images/node.png'))} alt=''/></div>
+                            <div className='name-node common-name'>Node</div>
                         </div>    
-                        <div className='box-mysql'>
-                            <img className='mysql back' src={(require('./images/mysql.png'))} alt=''/>
-                            <div className='name-mysql'>MySql</div>
-                        </div>
-                        <div className='box-mongodb'>
-                            <img className='mongodb back' src={(require('./images/mongodb.png'))} alt=''/>
-                            <div className='name-mongodb'>MongoDB</div>
-                        </div>      
+                        <div className='box-mysql common-box'>
+                            <div className='div-icon'><img className='mysql back' src={(require('./images/mysql.png'))} alt=''/></div>
+                            <div className='name-mysql common-name'>MySql</div>
+                        </div>     
                     </div>
-                    <div className="text-select-back">
-                        <div className='box-restfulapi'>
-                            <img className='restfulapi back' src={(require('./images/restfulapi.png'))} alt=''/>
-                            <div className='name-restfulapi'>RESTful API</div>
+                    <div className="text-select-all">
+                        <div className='box-restfulapi common-box'>
+                            <div className='div-icon'><img className='restfulapi back' src={(require('./images/restfulapi.png'))} alt=''/></div>
+                            <div className='name-restfulapi common-name'>RESTful API</div>
                         </div>
-                        <div className='box-strapi'>
-                            <img className='strapi back' src={(require('./images/strapi.png'))} alt=''/>
-                            <div className='name-strapi'>Strapi</div>
+                        <div className='box-strapi common-box'>
+                            <div className='div-icon'><img className='strapi back' src={(require('./images/strapi.png'))} alt=''/></div>
+                            <div className='name-strapi common-name'>Strapi</div>
                         </div>
+                        <div className='box-mongodb common-box'>
+                            <div className='div-icon'><img className='mongodb back' src={(require('./images/mongodb.png'))} alt=''/></div>
+                            <div className='name-mongodb common-name'>MongoDB</div>
+                        </div> 
                     </div>
                 </div>
                     
